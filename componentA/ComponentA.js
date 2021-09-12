168ae803089871b969e361a986ec63431e4c2002
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-09-12 07:29:28 +0300

    Add files via upload

---
+++ componentA/ComponentA.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ComponentA = () => {
+    return (
+        <ComponentAContainer>
+            <h2>ComponentA</h2>
+        </ComponentAContainer>
+    )
+}
+
+export default ComponentA
+
---
+++ componentA/ComponentAStyle.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ComponentAContainer = styled.div`
+background-color: red;
+
+
+
+
+
+`;
